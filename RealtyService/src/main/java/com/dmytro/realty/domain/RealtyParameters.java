f0e07d6ca3c21674b5c99070c24e9960f9caa3aa
Dmytro <dmytrozzz@i.ua>
2013-03-09 23:59:32 +0200

    Improved design. Replaced jstl login panel by facelet login panel.

--- RealtyService/src/main/java/com/dmytro/realty/domain/RealtyParameters.java
+++ RealtyService/src/main/java/com/dmytro/realty/domain/RealtyParameters.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "realty_parameters")
@@ -24,7 +25,14 @@ public class RealtyParameters implements Serializable {
 
     @Column(name = "price_to")
     private int toPrice = 5000;
-
+    
+    @Transient
+    private int fromRooms = 1;
+    @Transient
+    private int toRooms = 3;
+    @Transient
+    private boolean privateBusiness;    
+    
     public long getId() {
 	return id;
     }
@@ -47,10 +55,29 @@ public class RealtyParameters implements Serializable {
 
     public void setToPrice(int toPrice) {
 	this.toPrice = toPrice;
+    }       
+
+    public int getFromRooms() {
+        return fromRooms;
     }
 
-    @Override
-    public String toString() {
-	return "RealtyParameters [id=" + id + ", fromPrice=" + fromPrice + ", toPrice=" + toPrice + "]";
+    public void setFromRooms(int fromRooms) {
+        this.fromRooms = fromRooms;
+    }
+
+    public int getToRooms() {
+        return toRooms;
+    }
+
+    public void setToRooms(int toRooms) {
+        this.toRooms = toRooms;
     }        
+
+    public boolean isPrivateBusiness() {
+        return privateBusiness;
+    }
+
+    public void setPrivateBusiness(boolean privateBusiness) {
+        this.privateBusiness = privateBusiness;
+    }       
 }
