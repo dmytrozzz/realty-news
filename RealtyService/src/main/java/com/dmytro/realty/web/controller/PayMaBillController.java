9774924910ae3852188f3bdc118746ef8454f4b3
d.zonov <d.zonov@seclab.surc.kiev.ua>
2013-04-10 20:27:08 +0300

    Initial payment system integration. Footer adding

--- RealtyService/src/main/java/com/dmytro/realty/web/controller/PayMaBillController.java
+++ RealtyService/src/main/java/com/dmytro/realty/web/controller/PayMaBillController.java
@@ -1,20 +1,25 @@
 package com.dmytro.realty.web.controller;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.filter.HttpPutFormContentFilter;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller("payMaBillController")
 public class PayMaBillController {
 
-    @RequestMapping(value = "/user-pay/easy/success", method = RequestMethod.GET)
-    public void paySuccess(@RequestParam(value = "order_id") long orderId, @RequestParam(value = "amount") int amount,
-	    @RequestParam(value = "commission") String commision) {
-	System.out.println("Pay was made: " + orderId + "|" + amount + "|" + commision + "|");
-    }
+	@RequestMapping(value = "/user-pay/easy/success", method = RequestMethod.GET)
+	public void paySuccess(@RequestParam(value = "order_id") long orderId, @RequestParam(value = "amount") int amount,
+			@RequestParam(value = "commission") String commision) {
+		System.out.println("Pay was made: " + orderId + "|" + amount + "|" + commision + "|");
+		
+		
+	}
 
-    public void payError() {
-
-    }
+	public RedirectView payError() {
+		return new RedirectView("");
+	}
 }
