52e7a878ea3aa44f612d79628293402964096b4e
Dmytro <worker@com.ua>
2013-04-24 21:16:47 +0300

    Fixed a lot of things. Feedbacks system added. Marketing speech modified. Authentification feature for nav bar.

---
+++ RealtyService/src/main/java/com/dmytro/realty/domain/FeedBack.java
@@ -0,0 +1,64 @@
+package com.dmytro.realty.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "feedback")
+public class FeedBack implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "feedback_seq")
+	@SequenceGenerator(name = "feedback_seq", sequenceName = "feedback_id_seq", allocationSize = 1)
+	@Column(name = "id")
+	private long id;
+	
+	@Column(name = "author")
+	private String author;
+
+	@Column(name = "content")
+	private String content;
+	
+	@Column(name = "good")
+	private boolean good = true;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public boolean isGood() {
+		return good;
+	}
+
+	public void setGood(boolean good) {
+		this.good = good;
+	}
+	
+	
+}
