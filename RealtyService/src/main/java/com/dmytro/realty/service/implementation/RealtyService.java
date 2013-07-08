17e74a0b9610359bef4d1a268d6069e2c4408a8f
Dmytro <worker@com.ua>
2013-07-08 13:09:10 +0300

    Added cron delay only for normal hours of day

--- RealtyService/src/main/java/com/dmytro/realty/service/implementation/RealtyService.java
+++ RealtyService/src/main/java/com/dmytro/realty/service/implementation/RealtyService.java
@@ -2,7 +2,7 @@ package com.dmytro.realty.service.implementation;
 
 import com.dmytro.realty.data.repository.CriteriaRepository;
 import com.dmytro.realty.data.repository.ProxyRepository;
-import com.dmytro.realty.data.repository.UserRepository;
+import com.dmytro.realty.data.repository.RealtyOfferRepository;
 import com.dmytro.realty.domain.Proxy;
 import com.dmytro.realty.domain.RealtyCriteria;
 import com.dmytro.realty.engine.RealtyEngine;
@@ -12,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Repository
 @Transactional
@@ -23,26 +26,30 @@ public class RealtyService implements IRealtyService {
     @Autowired
     private ProxyRepository proxyRepository;
     @Autowired
-    private UserRepository userRepository;
+    private RealtyOfferRepository offerRepository;
+
     private RealtyEngine realtyEngine = new RealtyEngine();
 
     @Override
     public void searchRealty() {
-        Iterable<RealtyCriteria> criterias = criteriaRepository.findAllPayedAndEnabled();
-        RealtyService.proxy = proxyRepository.getRandom();
-        long start = System.currentTimeMillis();
-
-        for (RealtyCriteria criteria : criterias) {
-            proxy.setTries(proxy.getTries() + 1);
-            try {
-                realtyEngine.searchAndSubscribe(criteria, criteria.getUserCollection());
-            } catch (RealtyUnparsebleException e) {
-                System.out.println(e.getMessage());
-                proxy.setFailures(proxy.getFailures() + 1);
-            }
-        }
-        long time = (System.currentTimeMillis() - start) / 1000;
-        proxy.setSeconds((proxy.getSeconds() + (int) time) / 2);
-        proxyRepository.save(proxy);
+        System.out.println("11111111`1!!!!!" + new SimpleDateFormat("mm MM").format(new Date()));
+//        Iterable<RealtyCriteria> criterias = criteriaRepository.findAllPayedAndEnabled();
+//        //measure time for proxy job
+//        RealtyService.proxy = proxyRepository.getRandom();
+//        long start = System.currentTimeMillis();
+//        proxy.setTries(proxy.getTries() + 1);
+//
+//        //start cycle
+//        for (RealtyCriteria criteria : criterias) {
+//            try {
+//                realtyEngine.searchAndSubscribe(criteria, criteria.getUserCollection());
+//            } catch (RealtyUnparsebleException e) {
+//                System.out.println(e.getMessage());
+//                proxy.setFailures(proxy.getFailures() + 1);
+//            }
+//        }
+//        long time = (System.currentTimeMillis() - start) / 1000;
+//        proxy.setSeconds((proxy.getSeconds() + (int) time) / 2);
+//        proxyRepository.save(proxy);
     }
 }
